7129c88d1d600209867b93433d209142a7054b49
mg3442 <magnusprotas@gmail.com>
2018-10-06 15:40:13 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  const obj = {};
+  let count = 1;
+  let max = 0;
+  for( let i = 0; i < array.length; i++ ){
+
+    if( !obj[array[i]] ){
+
+        obj[array[i]] = true;
+    }
+}
+  
+  for( let pos in obj ){
+
+      if( obj[pos] && obj[( parseInt(pos) + 1 )] ){
+
+          count++;
+      }
+      else {
+
+          if(count > max){
+
+              max = count;
+          }
+          count = 1;
+      }
+  }
+
+  return max;
+
 }
